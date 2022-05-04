6af604c289a9665bf035f41cf707b6bdc7c561f7
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-04 11:27:15 +0300

    3 buttons storybook

--- src/components/Button.stories.jsx
+++ src/components/Button.stories.jsx
@@ -6,13 +6,18 @@ export default {
   component: Button,
 };
 
-export const Primary = () => (
+export const Primary = (props) => (
   <Button color='primary' type='submit'>
     Submit
   </Button>
 );
 export const Success = () => (
-  <Button color='success' type='reset'>
+  <Button color='success' type='button'>
+    Button
+  </Button>
+);
+export const Danger = () => (
+  <Button color='danger' type='reset'>
     Reset
   </Button>
 );
